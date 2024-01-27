b32b83a1ac701ce46cf04480fd9f86bf47122c62
surya <jayasurya12vj@gmail.com>
2024-01-27 13:21:26 +0530

    add getIndexValue and update

--- DSA/LinkedList/Demo.java
+++ DSA/LinkedList/Demo.java
@@ -1,8 +1,16 @@
 public class Demo {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.insertAtBeginng(9);
-        list.insertAtBeginng(5);
-        list.display();
+
+        list.insertAtBeginning(9);
+        list.insertAtBeginning(5);
+        list.insertAtBeginning(6);
+        list.insertAtBeginning(2);
+        
+        // list.display();
+        // list.deleteAtPos(0);
+        list.getIndexVal(2);
+        // list.updateAtPos(0, 10);
+        // list.display();
     }
 }
