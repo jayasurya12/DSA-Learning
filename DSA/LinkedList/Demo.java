66c2dc80791588ec280a790119677daf6bdc1375
surya <jayasurya12vj@gmail.com>
2024-01-27 19:33:00 +0530

    fix: add insert at last node method

--- DSA/LinkedList/Demo.java
+++ DSA/LinkedList/Demo.java
@@ -6,13 +6,14 @@ public class Demo {
         list.insertAtBeginning(5);
         list.insertAtBeginning(6);
         list.insertAtBeginning(2);
+        list.insertAtEnd(1000);
         // list.deleteAtPos(0);
         // list.getIndexVal(2);
         // list.updateAtPos(2, 10);
         // list.updateAtPosOfEnd(50);
         list.containsAtVal(510);
         list.searchVal(100);
-        list.deleteAtEnd();
+        // list.deleteAtEnd();
         list.display();
     }
 }
