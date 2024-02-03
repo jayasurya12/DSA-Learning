d3984a1ac931885fd04eee32e477fec5ea7524e0
jayasurya12 <jayasurya12vj@gmail.com>
2024-02-03 22:17:43 +0530

    fix: solved insert methods issues

--- DSA/Binary-Search-Tree/DemoBTS.java
+++ DSA/Binary-Search-Tree/DemoBTS.java
@@ -1,5 +1,23 @@
 public class DemoBTS {
     public static void main (String[] args) {
         BST bst = new BST(50);
+
+        bst.insert(bst.root, 20);
+        bst.insert(bst.root, 30);
+        bst.insert(bst.root, 40);
+        bst.insert(bst.root, 60);
+        bst.insert(bst.root, 70);
+        bst.insert(bst.root, 80);
+
+        bst.inOrder(bst.root);
+    
+        if (bst.search(bst.root, 70) == null)
+
+            System.out.print("Not found");
+
+        else
+
+            System.out.print("found");
+
     }
 }
