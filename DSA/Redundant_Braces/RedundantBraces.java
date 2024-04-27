f32ef786bb58fb77b453cca9203e5bffeb06bea8
surya <jayasurya12vj@gmail.com>
2024-04-27 12:48:17 +0530

    feat:Redundant Braces

---
+++ DSA/Redundant_Braces/RedundantBraces.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class RedundantBraces {
+    public boolean braces(String A) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0 ; i < A.length() ; i++) {
+            char c = A.charAt(i);
+            if(c == ' ')
+            continue;
+            if( c == '(' || c == '+' || c == '-' || c == '*' || c == '/')
+            s.push(c);
+            else if(c == ')') {
+                if (s.peek() == '(')
+                return false;
+                else {
+                    while(!s.isEmpty() && s.peek() != '(') {
+                        s.pop();
+                    }
+                    s.pop();
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        RedundantBraces r = new RedundantBraces();
+        System.out.println(r.braces("((a+b))"));
+    }
+}
