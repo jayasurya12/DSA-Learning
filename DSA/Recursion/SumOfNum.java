16043ef1b60f65d1b5c0356eac45187177e9e17d
jayasurya <jayasurya@atatus.com>
2024-07-13 18:11:09 +0530

    add recursion problems return sum method

--- DSA/Recursion/SumOfNum.java
+++ DSA/Recursion/SumOfNum.java
@@ -6,7 +6,15 @@ class SumOfNum {
         }
         sum(num - 1, sum + num);  // Add num to sum in the recursive call
     }
+
+    static int sum(int num){
+        if (num == 0) return 0;
+        return num + sum(num-1);
+    }
+
     public static void main(String[] args) {
         sum(3, 0);
+        System.out.println("Sum : "+ sum(3));
     }
+
 }
