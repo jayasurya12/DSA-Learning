7cb33756c5c51d74831b135da741cdb071cf41e0
jayasurya <jayasurya@atatus.com>
2024-07-14 12:00:00 +0530

    feat: add palindrom

---
+++ DSA/Recursion/Recursion.java
@@ -0,0 +1,53 @@
+class Recursion {
+    static void sum(int num, int sum) {
+        if (num < 1) {
+            System.out.println(sum);
+            return;
+        }
+        sum(num - 1, sum + num);  // Add num to sum in the recursive call
+    }
+
+    static int sum(int num){
+        if (num == 0) return 0;
+        return num + sum(num-1);
+    }
+
+    static int fact(int num) {
+        if (num == 1) return 1;
+        return num * fact(num - 1);
+        //TC O(N) and SC O(N);
+    }
+
+    static void reverse(int l, int[] arr, int r) {
+        if (l >= r) return;
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        reverse(l + 1, arr, r - 1);
+    }
+    static boolean palindrom (String str, int l, int r) {
+        if (str.length() <= 2) {
+            return true;
+        };
+        if (l >= r) return true;
+        if (str.charAt(l) != str.charAt(r)) return false;
+        return palindrom(str, l+1, r-1);
+    }
+    public static void main(String[] args) {
+        sum(3, 0);
+        System.out.println("Sum : "+ sum(3));
+
+        System.out.println("Sum of factorial: " + fact(5));
+
+        int[] arr = {4, 3, 2, 1, 0};
+        reverse(0, arr, arr.length - 1);  // Corrected to use arr.length - 1
+
+        for (int num : arr) {
+            System.out.print(num + " ");  // Corrected to print the elements directly
+        }
+        System.out.println(" ");
+        String pal = "malayalam";
+        System.out.println("'"+pal+"'" + " is palindrom : " +palindrom(pal, 0, pal.length()-1));
+    }
+
+}
